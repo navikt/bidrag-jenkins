e78d44e500c7403a7aac39f2700e5227fa2eab9d
Bjorn Stadheim <stadheimb@gmail.com>
2018-12-11 14:37:27 +0100

    hack n slash

--- src/no/nav/bidrag/dokument/Builder.groovy
+++ src/no/nav/bidrag/dokument/Builder.groovy
@@ -6,21 +6,21 @@ class Builder {
 
         if (isSnapshot) {
             "echo \"running maven build image.\"".execute()
-            "docker run --rm -v `pwd`:/usr/src/mymaven -w /usr/src/mymaven -v \"${home}/.m2\":/root/.m2 ${mvnImage} mvn clean install -B -e"
+            "docker run --rm -v `pwd`:/usr/src/mymaven -w /usr/src/mymaven -v \"${home}/.m2\":/root/.m2 ${mvnImage} mvn clean install -B -e".execute()
         } else {
             println("POM version is not a SNAPSHOT, it is ${pomVersion}. Skipping build and testing of backend")
         }
     }
 
-    static def releaseArtifact(isSnapshot, mvnImage, imageVersion, releaseVersion, environment, application, pomversion, home, dockerRepo) {
+    static def releaseArtifact(script, isSnapshot, mvnImage, imageVersion, releaseVersion, environment, application, pomversion, home, dockerRepo) {
         if (isSnapshot) {
-            sh "docker run --rm -v `pwd`:/usr/src/mymaven -w /usr/src/mymaven -v '${home}/.m2':/root/.m2 ${mvnImage} mvn versions:set -B -DnewVersion=${releaseVersion} -DgenerateBackupPoms=false"
-            sh "docker run --rm -v `pwd`:/usr/src/mymaven -w /usr/src/mymaven -v '${home}/.m2':/root/.m2 ${mvnImage} mvn clean install -DskipTests -Dhendelse.environments=${environment} -B -e"
-            sh "docker build --build-arg version=${releaseVersion} -t ${dockerRepo}/${application}:${imageVersion} ."
-            sh "git commit -am \"set version to ${releaseVersion} (from Jenkins pipeline)\""
-            sh "git push"
-            sh "git tag -a ${application}-${releaseVersion}-${environment} -m ${application}-${releaseVersion}-${environment}"
-            sh "git push --tags"
+            script.sh "docker run --rm -v `pwd`:/usr/src/mymaven -w /usr/src/mymaven -v '${home}/.m2':/root/.m2 ${mvnImage} mvn versions:set -B -DnewVersion=${releaseVersion} -DgenerateBackupPoms=false"
+            script.sh "docker run --rm -v `pwd`:/usr/src/mymaven -w /usr/src/mymaven -v '${home}/.m2':/root/.m2 ${mvnImage} mvn clean install -DskipTests -Dhendelse.environments=${environment} -B -e"
+            script.sh "docker build --build-arg version=${releaseVersion} -t ${dockerRepo}/${application}:${imageVersion} ."
+            script.sh "git commit -am \"set version to ${releaseVersion} (from Jenkins pipeline)\""
+            script.sh "git push"
+            script.sh "git tag -a ${application}-${releaseVersion}-${environment} -m ${application}-${releaseVersion}-${environment}"
+            script.sh "git push --tags"
         } else {
             println("POM version is not a SNAPSHOT, it is ${pomversion}. Skipping releasing")
         }
