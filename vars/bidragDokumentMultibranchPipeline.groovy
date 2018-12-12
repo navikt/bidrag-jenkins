ff265171830d20d3f9a6111e3fc2c26a6d25881a
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2018-12-12 11:47:42 +0100

    BD-290: multibranch

---
+++ vars/bidragDokumentMultibranchPipeline.groovy
@@ -0,0 +1,25 @@
+
+def call(body) {
+
+    def pipelineParams = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    println "bidragDokumentmMultibranchPipeline: pipelineParams = ${pipelineParams}"
+
+    application = pipelineParams.application
+    branch = pipelineParams.branch
+    mvnImage = pipelineParams.mvnImage
+    environment = pipelineParams.environment
+    dockerRepo = "repo.adeo.no:5443"
+    nais = "/usr/bin/nais"
+    appConfig = "nais.yaml"
+    cluster = "${naisCluster}"
+
+    node {
+        stage("prepare multibranch shared library pipeline") {
+            println "running multibranch: `pwd`"
+        }
+    }
+}
