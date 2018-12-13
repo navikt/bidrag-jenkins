42d44d51a7d31c92e2f257fb6ad0f84214cbef3e
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2018-12-13 12:01:32 +0100

    baby steps

--- vars/bidragDokumentMultibranchPipeline.groovy
+++ vars/bidragDokumentMultibranchPipeline.groovy
@@ -15,9 +15,16 @@ def call(body) {
     dockerRepo = "repo.adeo.no:5443"
     nais = "/usr/bin/nais"
     appConfig = "nais.yaml"
+    labelOfBuild = application + '_test'
 //    cluster = "${naisCluster}"
 
-    node {
+    pipeline {
+        agent {
+            docker: mvnImage
+            label: labelOfBuild
+            args: -v /var/lib/jenkins/workspace/bidrag-dokument-dto
+        }
+
         stage("prepare multibranch shared library pipeline") {
             sh 'env'
             println "running multibranch: $PWD"
