2111b3cafe20ed9e34d71ea666c84e5d48c89915
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2018-12-19 15:34:42 +0100

    BD-297: baby steps -> nais deploy

--- src/no/nav/bidrag/dokument/GitHubArtifact.groovy
+++ src/no/nav/bidrag/dokument/GitHubArtifact.groovy
@@ -20,7 +20,7 @@ class GitHubArtifact {
         pipelineEnvironment.multibranchPipeline.cleanWs()
         pipelineEnvironment.multibranchPipeline.withCredentials([pipelineEnvironment.multibranchPipeline.string(credentialsId: 'OAUTH_TOKEN', variable: 'token')]) {
             pipelineEnvironment.multibranchPipeline.withEnv(['HTTPS_PROXY=http://webproxy-utvikler.nav.no:8088']) {
-                pipelineEnvironment.multibranchPipeline.sh(script: "git clone https://${token}:x-oauth-basic@github.com/navikt/${gitHubProjectName}.git .")
+                pipelineEnvironment.multibranchPipeline.sh(script: "git clone https://${pipelineEnvironment.multibranchPipeline.token}:x-oauth-basic@github.com/navikt/${gitHubProjectName}.git .")
                 pipelineEnvironment.multibranchPipeline.sh "echo '****** BRANCH ******'"
                 pipelineEnvironment.multibranchPipeline.sh "echo 'BRANCH CHECKOUT: ${branch}'......"
                 pipelineEnvironment.multibranchPipeline.sh(script: "git checkout ${branch}")
@@ -37,8 +37,7 @@ class GitHubArtifact {
     }
 
     private def readPomFromSourceCode() {
-        String workspace = pipelineEnvironment.workspace
-        execute("echo", "parsing pom.xml from ${workspace}")
+        execute("echo", "parsing pom.xml from ${pipelineEnvironment.workspace}")
         def pom = pipelineEnvironment.multibranchPipeline.readMavenPom file: 'pom.xml'
 
         return pom
@@ -80,23 +79,20 @@ class GitHubArtifact {
 
     boolean isLastCommitterFromPipeline() {
         pipelineEnvironment.lastCommitter = pipelineEnvironment.multibranchPipeline.sh(script: 'git log -1 --pretty=format:"%an (%ae)"', returnStdout: true).trim()
-        String lastCommitter = pipelineEnvironment.lastCommitter
-        execute("echo", "last commit done by $lastCommitter")
+        execute("echo", "last commit done by ${pipelineEnvironment.lastCommitter}")
 
-        return lastCommitter.contains('navikt-ci')
+        return pipelineEnvironment.lastCommitter.contains('navikt-ci')
     }
 
     void updateMinorVersion() {
         String homeFolderInJenkins = pipelineEnvironment.homeFolderJenkins
-        String lastCommitter = pipelineEnvironment.lastCommitter
         String majorVersion = fetchMajorVersion()
         String minorVersion = fetchMinorVersion()
         String mvnImage = pipelineEnvironment.mvnImage
         String nextVersion = "${majorVersion}." + (minorVersion.toInteger() + 1) + "-SNAPSHOT"
-        String workspace = pipelineEnvironment.workspace
 
-        pipelineEnvironment.multibranchPipeline.sh "docker run --rm -v ${workspace}:/usr/src/mymaven -w /usr/src/mymaven -v '$homeFolderInJenkins/.m2':/root/.m2 ${mvnImage} mvn versions:set -B -DnewVersion=${nextVersion} -DgenerateBackupPoms=false"
-        pipelineEnvironment.multibranchPipeline.sh "git commit -a -m \"updated to new minor version ${nextVersion} after release by ${lastCommitter}\""
+        pipelineEnvironment.multibranchPipeline.sh "docker run --rm -v ${pipelineEnvironment.workspace}:/usr/src/mymaven -w /usr/src/mymaven -v '$homeFolderInJenkins/.m2':/root/.m2 ${mvnImage} mvn versions:set -B -DnewVersion=${nextVersion} -DgenerateBackupPoms=false"
+        pipelineEnvironment.multibranchPipeline.sh "git commit -a -m \"updated to new minor version ${nextVersion} after release by ${pipelineEnvironment.lastCommitter}\""
         pipelineEnvironment.multibranchPipeline.sh "git push"
     }
 
@@ -108,13 +104,11 @@ class GitHubArtifact {
         // only bump major version if not previously bumped...
         if (masterMajorVersion == developMajorVersion) {
             String developMinorVersion = fetchMinorVersion(readPomFromSourceCode())
-            String lastCommitter = pipelineEnvironment.lastCommitter
             String mvnImage = pipelineEnvironment.mvnImage
             String nextVersion = (masterMajorVersion.toFloat() + 1) + ".${developMinorVersion}-SNAPSHOT"
-            String workspace = pipelineEnvironment.workspace
 
-            pipelineEnvironment.multibranchPipeline.sh "docker run --rm -v ${workspace}:/usr/src/mymaven -w /usr/src/mymaven -v '$homeFolderInJenkins/.m2':/root/.m2 ${mvnImage} mvn versions:set -B -DnewVersion=${nextVersion} -DgenerateBackupPoms=false"
-            pipelineEnvironment.multibranchPipeline.sh "git commit -a -m \"updated to new major version ${nextVersion} after release by ${lastCommitter}\""
+            pipelineEnvironment.multibranchPipeline.sh "docker run --rm -v ${pipelineEnvironment.workspace}:/usr/src/mymaven -w /usr/src/mymaven -v '$homeFolderInJenkins/.m2':/root/.m2 ${mvnImage} mvn versions:set -B -DnewVersion=${nextVersion} -DgenerateBackupPoms=false"
+            pipelineEnvironment.multibranchPipeline.sh "git commit -a -m \"updated to new major version ${nextVersion} after release by ${pipelineEnvironment.lastCommitter}\""
             pipelineEnvironment.multibranchPipeline.sh "git push"
         }
     }
