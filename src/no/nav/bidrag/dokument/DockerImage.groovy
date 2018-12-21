af8bac5c3156ce3fd3f90b1eac2af26c9220650d
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2018-12-21 15:07:08 +0100

    BD-297: tag with prod when master...

--- src/no/nav/bidrag/dokument/DockerImage.groovy
+++ src/no/nav/bidrag/dokument/DockerImage.groovy
@@ -26,11 +26,15 @@ class DockerImage {
             pipelineEnvironment.execute "docker run --rm -v ${workspaceFolder}:/usr/src/mymaven -w /usr/src/mymaven -v '${pipelineEnvironment.homeFolderJenkins}/.m2':/root/.m2 ${pipelineEnvironment.mvnImage} mvn versions:set -B -DnewVersion=${pipelineEnvironment.mvnVersion} -DgenerateBackupPoms=false"
             pipelineEnvironment.execute "docker run --rm -v ${workspaceFolder}:/usr/src/mymaven -w /usr/src/mymaven -v '${pipelineEnvironment.homeFolderJenkins}/.m2':/root/.m2 ${pipelineEnvironment.mvnImage} mvn clean install -DskipTests -Dhendelse.environments=${pipelineEnvironment.fetchEnvironment()} -B -e"
 
-            if (!pipelineEnvironment.isMaster || pipelineEnvironment.fetchEnvironment() != 'q0') {
-                pipelineEnvironment.execute "docker build --build-arg version=${pipelineEnvironment.mvnVersion} -t ${pipelineEnvironment.dockerRepo}/${pipelineEnvironment.gitHubProjectName}:${pipelineEnvironment.fetchImageVersion()} ."
-                pipelineEnvironment.execute "git tag -a ${pipelineEnvironment.gitHubProjectName}-${pipelineEnvironment.mvnVersion}-${pipelineEnvironment.fetchEnvironment()} -m ${pipelineEnvironment.gitHubProjectName}-${pipelineEnvironment.mvnVersion}-${pipelineEnvironment.fetchEnvironment()}"
-                pipelineEnvironment.execute "git push --tags"
+            String tmpTag = pipelineEnvironment.fetchEnvironment()
+
+            if (pipelineEnvironment.isMaster && tmpTag == 'q0') {
+                tmpTag = 'prod'
             }
+
+            pipelineEnvironment.execute "docker build --build-arg version=${pipelineEnvironment.mvnVersion} -t ${pipelineEnvironment.dockerRepo}/${pipelineEnvironment.gitHubProjectName}:${pipelineEnvironment.fetchImageVersion()} ."
+            pipelineEnvironment.execute "git tag -a ${pipelineEnvironment.gitHubProjectName}-${pipelineEnvironment.mvnVersion}-${tmpTag} -m ${pipelineEnvironment.gitHubProjectName}-${pipelineEnvironment.mvnVersion}-${tmpTag}"
+            pipelineEnvironment.execute "git push --tags"
         } else {
             pipelineEnvironment.println("POM version is not a SNAPSHOT, it is ${pipelineEnvironment.mvnVersion}. Skipping releasing")
         }
