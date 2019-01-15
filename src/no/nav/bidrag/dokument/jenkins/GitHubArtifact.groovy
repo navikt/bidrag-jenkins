f833f88a36fdc8f0320f1350620e819e9105ba37
Bjorn Stadheim <stadheimb@gmail.com>
2019-01-15 12:50:28 +0100

    Temp fix for jenkins build

--- src/no/nav/bidrag/dokument/jenkins/GitHubArtifact.groovy
+++ src/no/nav/bidrag/dokument/jenkins/GitHubArtifact.groovy
@@ -12,9 +12,11 @@ abstract class GitHubArtifact {
         String gitHubProjectName = pipelineEnvironment.gitHubProjectName
 
         pipelineEnvironment.buildScript.cleanWs()
-        pipelineEnvironment.buildScript.withCredentials([pipelineEnvironment.buildScript.string(credentialsId: 'OAUTH_TOKEN', variable: 'token')]) {
+        // pipelineEnvironment.buildScript.withCredentials([pipelineEnvironment.buildScript.string(credentialsId: 'OAUTH_TOKEN', variable: 'token')]) {
+        pipelineEnvironment.buildScript.withCredentials(
+                [[$class: 'UsernamePasswordMultiBinding', credentialsId: 'jenkinsPipeline', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]
             pipelineEnvironment.buildScript.withEnv(['HTTPS_PROXY=http://webproxy-utvikler.nav.no:8088']) {
-                pipelineEnvironment.buildScript.sh(script: "git clone https://${pipelineEnvironment.buildScript.token}:x-oauth-basic@github.com/navikt/${gitHubProjectName}.git .")
+                pipelineEnvironment.buildScript.sh(script: "git clone https://${pipelineEnvironment.buildScript.USERNAME}:${pipelineEnvironment.buildScript.PASSWORD}@github.com/navikt/${gitHubProjectName}.git .")
                 pipelineEnvironment.buildScript.sh "echo '****** BRANCH ******'"
                 pipelineEnvironment.buildScript.sh "echo 'BRANCH CHECKOUT: ${branch}'......"
                 pipelineEnvironment.buildScript.sh(script: "git checkout ${branch}")
