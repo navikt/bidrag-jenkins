48176fe765698e33ecc8c50994e88c9f4928fe37
Bjorn Stadheim <stadheimb@gmail.com>
2019-02-08 12:54:49 +0100

    Initial boilerplate for new cucumber test runs

--- src/no/nav/bidrag/jenkins/Cucumber.groovy
+++ src/no/nav/bidrag/jenkins/Cucumber.groovy
@@ -12,38 +12,41 @@ class Cucumber {
 
         // Only throw an exception when cucumber miss json file, else only fail this step
         try {
-            if (pipelineEnvironment.fileExists('cucumber')) {
-                pipelineEnvironment.println("[INFO] Run cucumber tests")
-                sleep(10000)
+            pipelineEnvironment.println("[INFO] Run cucumber tests")
+            sleep(10000)
 
-                try {
-                    pipelineEnvironment.buildScript.withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'naisUploader', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-                        pipelineEnvironment.execute(
-                            "docker run --rm -e environment=${pipelineEnvironment.fetchEnvironment()} " + 
-                            "-e fasit_user=${pipelineEnvironment.buildScript.USERNAME} -e fasit_pass='${pipelineEnvironment.buildScript.PASSWORD}' " +
-                            "-v ${pipelineEnvironment.workspace}/cucumber:/cucumber bidrag-dokument-cucumber"
-                        )
-                    }
-                } catch (Exception e) {
-                    pipelineEnvironment.println('Unstable build: ' + e)
-                    result = 'UNSTABLE'
-                }
+            pipelineEnvironment.buildScript.withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'naisUploader', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
 
-                if (pipelineEnvironment.fileExists('cucumber/cucumber.json')) {
-                    pipelineEnvironment.buildScript.cucumber buildStatus: 'UNSTABLE',
-                            fileIncludePattern: 'cucumber/*.json',
-                            trendsLimit: 10,
-                            classifications: [
-                                    [
-                                            'key'  : 'Browser',
-                                            'value': 'Firefox'
-                                    ]
-                            ]
-                } else {
-                    throw new IllegalStateException("No cucumber.json in cucumber folder")
+            try {
+                // Checkout bidrag-cucumber -> ./bidrag-cucumber
+                // GitHubArtifact will do the magic of matching current branch to bidrag-cucumber branch
+                pipelineEnvironment.initGitHubArtifact().checkoutCucumber(pipelineEnvironment.branchName)
+
+                // Instead of linking ./cucumber to the container link the project specific sub-dir to run test for current project (-v param)
+                pipelineEnvironment.buildScript.withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'naisUploader', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+                    pipelineEnvironment.execute(
+                        "docker run --rm -e environment=${pipelineEnvironment.fetchEnvironment()} " + 
+                        "-e fasit_user=${pipelineEnvironment.buildScript.USERNAME} -e fasit_pass='${pipelineEnvironment.buildScript.PASSWORD}' " +
+                        "-v ${pipelineEnvironment.workspace}/bidrag-cucumber/cucumber/${application}:/cucumber bidrag-dokument-cucumber"
+                    )
                 }
+            } catch (Exception e) {
+                pipelineEnvironment.println('Unstable build: ' + e)
+                result = 'UNSTABLE'
+            }
+
+            if (pipelineEnvironment.fileExists('cucumber/cucumber.json')) {
+                pipelineEnvironment.buildScript.cucumber buildStatus: 'UNSTABLE',
+                        fileIncludePattern: 'cucumber/*.json',
+                        trendsLimit: 10,
+                        classifications: [
+                                [
+                                        'key'  : 'Browser',
+                                        'value': 'Firefox'
+                                ]
+                        ]
             } else {
-                pipelineEnvironment.println("[INFO] No cucumber directory - no tests to run!")
+                throw new IllegalStateException("No cucumber.json in cucumber folder")
             }
         } catch (Exception e) {
             pipelineEnvironment.println("Failed build: " + e)
