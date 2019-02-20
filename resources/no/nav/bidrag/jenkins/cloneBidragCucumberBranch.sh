e8d43d7068fafc12fe4bc20a1e519d9493e73320
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-02-20 11:14:57 +0100

    full path to cloneBidragCucumberBranch.sh, take two

--- resources/no/nav/bidrag/jenkins/cloneBidragCucumberBranch.sh
+++ resources/no/nav/bidrag/jenkins/cloneBidragCucumberBranch.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 
-function cloneBidragCucumberBranch() {
-  git clone https://$1:$2@github.com/navikt/bidrag-cucumber.git bidrag-cucumber
-  STATUS=$?
+git clone https://$1:$2@github.com/navikt/bidrag-cucumber.git bidrag-cucumber
+STATUS=$?
 
-  if [[ ${STATUS} -eq 0 ]]
-    then
-    cd bidrag-cucumber
-    echo 'pwd:' && pwd
-    echo '****** BRANCH ******'
-    echo 'BRANCH CHECKOUT: $2......'
+if [[ ${STATUS} -eq 0 ]]
+  then
+  cd bidrag-cucumber
+  echo 'pwd:' && pwd
+  echo '****** BRANCH ******'
+  echo 'BRANCH CHECKOUT: $2......'
 
-    git checkout $2
-    exit 0
-  fi
+  git checkout $2
+  exit 0
+fi
 
-  exit ${STATUS}
-}
+exit ${STATUS}
--- src/no/nav/bidrag/jenkins/GitHubArtifact.groovy
+++ src/no/nav/bidrag/jenkins/GitHubArtifact.groovy
@@ -32,8 +32,8 @@ abstract class GitHubArtifact {
         pipelineEnvironment.buildScript.withCredentials(
                 [[$class: 'UsernamePasswordMultiBinding', credentialsId: 'jenkinsPipeline', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
             pipelineEnvironment.buildScript.withEnv(['HTTPS_PROXY=http://webproxy-utvikler.nav.no:8088']) {
-                pipelineEnvironment.buildScript.sh(script:
-                        "./cloneBidragCucumberBranch.sh ${pipelineEnvironment.buildScript.USERNAME} ${pipelineEnvironment.buildScript.PASSWORD} ${branch}"
+                pipelineEnvironment.buildScript.sh(script: "${pipelineEnvironment.workspace}@libs/bidrag-jenkins/resources/no/nav/bidrag/jenkins/cloneBidragCucumberBranch.sh " +
+                        "${pipelineEnvironment.buildScript.USERNAME} ${pipelineEnvironment.buildScript.PASSWORD} ${branch}"
                 )
             }
         }
