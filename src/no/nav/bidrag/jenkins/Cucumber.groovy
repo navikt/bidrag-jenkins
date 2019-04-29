c3fde356c5e580d504f039f3c90c2ca49c068d97
Bjorn Stadheim <stadheimb@gmail.com>
2019-04-29 10:37:25 +0200

    Vent på deploy av nye pods før cucumber tester kjøres

--- src/no/nav/bidrag/jenkins/Cucumber.groovy
+++ src/no/nav/bidrag/jenkins/Cucumber.groovy
@@ -7,13 +7,70 @@ class Cucumber {
         this.pipelineEnvironment = pipelineEnvironment
     }
 
+    boolean waitForCurrentBuildToDeploy() {
+        String app = pipelineEnvironment.gitHubProjectName
+        String currentImageVersion = pipelineEnvironment.fetchImageVersion()
+        String ns = pipelineEnvironment.fetchNamespace()
+        Integer sleepInterval = 5000
+        Integer maxRetries = 20
+
+        while(maxRetries-- > 0) {
+
+            pipelineEnvironment.println "#${maxRetries} Checking POD status for app ${app}"
+
+            /*
+            NAME                               READY     STATUS    RESTARTS   AGE
+            bidrag-dokument-8668bc66c9-8hcf7   1/1       Running   0          20m
+            bidrag-dokument-8668bc66c9-mr5b8   1/1       Running   0          19m
+            */
+            Integer oldpods = 0
+            Integer newpods = 0
+            String str = pipelineEnvironment.buildScript.sh(script: "kubectl -n ${ns} get pod -l app=${app}", returnStdout:true)
+            str.tokenize("\n").each {
+                List line = it.tokenize(" ")
+                if(line.size() == 5) {
+                    String podId = line.get(0)
+                    String status = line.get(2)
+                    if(status == "Running") {
+                        String desc = pipelineEnvironment.buildScript.sh(script: "kubectl -n ${ns} describe pod ${podId}", returnStdout:true)
+                        desc.tokenize("\n").each {
+                            // APP_VERSION:                   1.0.176-SNAPSHOT-q0-16899879708
+                            if(it.trim().startsWith("APP_VERSION:")) {
+                                String appVersion = it.tokenize(':').get(1).trim()
+                                if(appVersion != currentImageVersion) {
+                                    pipelineEnvironment.println "Fant ${podId} med gammel APP_VERSION ${appVersion}"
+                                    oldpods++
+                                } else {
+                                    pipelineEnvironment.println "Fant ${podId} med forventet APP_VERSION ${appVersion}"
+                                    newpods++
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            // Vent til alle gamle poder er stoppet og minst en ny går før retur
+            if(oldpods == 0 && newpods > 0) {
+                return true
+            }
+            println "Sleeping ${sleepInterval}"
+            sleep(sleepInterval)
+        }
+        return false
+    }
+
     String runCucumberTests() {
         String result = 'SUCCESS'
 
         // Only throw an exception when cucumber miss json file, else only fail this step
         pipelineEnvironment.println("[INFO] Run cucumber tests")
-        sleep(10000)
 
+        pipelineEnvironment.buildScript.withEnv(['HTTPS_PROXY=http://webproxy-utvikler.nav.no:8088',
+            'NO_PROXY=localhost,127.0.0.1,10.33.43.41,.local,.adeo.no,.nav.no,.devillo.no,.oera.no,.nais.preprod.local,.nais-iapp.preprod.local,.nais.oera-q.local']) {
+            if(!waitForCurrentBuildToDeploy()) {
+                throw new IllegalStateException("Timeout waiting for current build to deploy")
+            }
+        }
         try {
             pipelineEnvironment.buildScript.withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'naisUploader', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
                 try {
