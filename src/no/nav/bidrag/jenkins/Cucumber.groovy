a0640e0fd666ac4f58b8c600f1c066697e0e62bb
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-10-31 14:26:13 +0100

    writing cucumber reports will wait for another day

--- src/no/nav/bidrag/jenkins/Cucumber.groovy
+++ src/no/nav/bidrag/jenkins/Cucumber.groovy
@@ -1,7 +1,5 @@
 package no.nav.bidrag.jenkins
 
-import no.nav.bidrag.jenkins.maven.MavenBuilder
-
 class Cucumber {
     private PipelineEnvironment pipelineEnvironment
 
@@ -73,21 +71,21 @@ class Cucumber {
     String runCucumberBackendTests() {
         pipelineEnvironment.checkoutCucumberBackendFeatureOrUseMaster()
 
-        try {
+//        try {
             pipelineEnvironment.executeMavenTest()
-        } catch (err) {  // Failures should not terminate the pipeline
-            println("SOMETHING FISHY HAPPENED: " + err)
-            return "UNSTABLE"
-        }
-
-        pipelineEnvironment.buildScript.cucumber buildStatus: 'UNSTABLE',
-                fileIncludePattern: '../bidrag-cucumber-backend/target/cucumber-report/cucumber.json',
-                trendsLimit: 10,
-                classifications: [
-                        [
-                                'key'  : 'Browser',
-                                'value': 'Firefox'
-                        ]
-                ]
+//        } catch (err) {  // Failures should not terminate the pipeline
+//            println("SOMETHING FISHY HAPPENED: " + err)
+//            return "UNSTABLE"
+//        }
+//
+//        pipelineEnvironment.buildScript.cucumber buildStatus: 'UNSTABLE',
+//                fileIncludePattern: '../bidrag-cucumber-backend/target/cucumber-report/cucumber.json',
+//                trendsLimit: 10,
+//                classifications: [
+//                        [
+//                                'key'  : 'Browser',
+//                                'value': 'Firefox'
+//                        ]
+//                ]
     }
 }
