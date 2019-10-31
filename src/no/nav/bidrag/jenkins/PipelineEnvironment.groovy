99717bead7cdae0eaf6ecfba227c6c3ca5d223e4
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-10-31 13:33:18 +0100

    upper case -> lower case

--- src/no/nav/bidrag/jenkins/PipelineEnvironment.groovy
+++ src/no/nav/bidrag/jenkins/PipelineEnvironment.groovy
@@ -194,7 +194,7 @@ class PipelineEnvironment {
 
         buildScript.withCredentials([
                 buildScript.usernamePassword(credentialsId: 'j104364', usernameVariable: 'USERNAME', passwordVariable: 'USER_AUTH'),
-                buildScript.usernamePassword(credentialsId: 'Z992903', usernameVariable: 'TEST_USER', passwordVariable: 'TEST_PASS')
+                buildScript.usernamePassword(credentialsId: 'z992903', usernameVariable: 'TEST_USER', passwordVariable: 'TEST_PASS')
         ]) {
             new MavenBuilder(this).executeMavenTest(
                     path_cucumber,
