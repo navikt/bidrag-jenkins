3049f880f02b54773a51d26084f686b4ce4996e5
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-01-23 15:53:52 +0100

    added jenkinsfile and pipeline for prod

---
+++ vars/naiseratorProdPipeline.groovy
@@ -0,0 +1,101 @@
+import no.nav.bidrag.jenkins.*
+
+def call(body) {
+
+    def pipelineParams = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    println "naisPipeline: pipelineParams = ${pipelineParams}"
+
+    PipelineEnvironment pipelineEnvironment = new PipelineEnvironment(
+            pipelineParams.gitHubProjectName,
+            pipelineParams.buildImage,
+            pipelineParams.environmentInDevelopBranch,
+            pipelineParams.buildType
+    )
+
+    Builder builder = pipelineEnvironment.initBuilder()
+    Cucumber cucumber = new Cucumber(pipelineEnvironment)
+    DockerImage dockerImage = new DockerImage(pipelineEnvironment)
+    GitHubArtifact gitHubArtifact = pipelineEnvironment.initGitHubArtifact()
+    Nais nais = new Nais(pipelineEnvironment)
+
+    pipeline {
+        agent any
+
+        stages {
+            stage("init environment") {
+                steps {
+                    script {
+                        sh 'env'
+                        pipelineEnvironment.branchName = "$BRANCH_NAME"
+                        pipelineEnvironment.homeFolderJenkins = "$HOME"
+                        pipelineEnvironment.buildScript = this
+                        pipelineEnvironment.path_jenkins_workspace = "$JENKINS_HOME/workspace"
+                        pipelineEnvironment.path_cucumber = "$WORKSPACE/bidrag-cucumber-backend"
+                        pipelineEnvironment.path_workspace = "$WORKSPACE"
+
+                        boolean isAutomatedBuild = currentBuild.rawBuild.getCause(hudson.model.Cause$UserIdCause) == null
+
+                        if (isAutomatedBuild && gitHubArtifact.isLastCommitterFromPipeline()) {
+                            pipelineEnvironment.doNotRunPipeline("$BUILD_ID")
+                        } else {
+                            gitHubArtifact.checkout("$BRANCH_NAME")
+                            pipelineEnvironment.artifactVersion = gitHubArtifact.fetchVersion()
+                            pipelineEnvironment.checkoutCucumberBackendFeatureOrUseMaster()
+                        }
+                    }
+                }
+            }
+
+            stage("Verify dependency versions") {
+                when { expression { pipelineEnvironment.canRunPipeline } }
+                steps { script { builder.verifySnapshotDependencies(gitHubArtifact.fetchBuildDescriptor()) } }
+            }
+
+            stage("build and test") {
+                when { expression { pipelineEnvironment.canRunPipeline } }
+                steps { script { builder.buildAndTest() } }
+            }
+
+            // major/minor versions are always edited manually: todo: tag final versions when master...
+            stage("bump patch version on develop") {
+                when { expression { pipelineEnvironment.canRunPipelineOnDevelop(gitHubArtifact.isLastCommitterFromPipeline()) } }
+                steps { script { gitHubArtifact.updatePatchVersion(builder) } }
+            }
+
+            stage("release and publish docker image") {
+                when { expression { pipelineEnvironment.canRunPipeline } }
+                steps { script { dockerImage.releaseAndPublish() } }
+            }
+
+            stage("apply naiserator") {
+                when { expression { pipelineEnvironment.canRunPipeline } }
+                steps { script { nais.applyNaiserator() } }
+            }
+
+            stage("wait for deploy and termination of old pods") {
+                when { expression { pipelineEnvironment.canRunPipeline } }
+                steps { script { nais.waitForNaiseratorDeployAndOldPodsTerminated() } }
+            }
+
+            stage("run cucumber tests for backend") {
+                when { expression { pipelineEnvironment.canRunPipelineWithMaven() } }
+                steps { script { result = cucumber.runCucumberBackendTests() } }
+            }
+        }
+
+        post {
+            always {
+                script {
+                    gitHubArtifact.resetWorkspace()
+                    gitHubArtifact.resetCucumber()
+                    dockerImage.deleteImagesNotUsed()
+                    pipelineEnvironment.deleteBuildWhenPipelineIsNotExecuted(Jenkins.instance.items)
+                }
+            }
+        }
+    }
+}
