8736166c7f819cd018c7dc0f871a277be3d4f3f7
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-01-29 14:04:33 +0100

    remove debug version

--- src/no/nav/bidrag/jenkins/DockerImage.groovy
+++ src/no/nav/bidrag/jenkins/DockerImage.groovy
@@ -25,8 +25,6 @@ class DockerImage {
 
     void releaseAndPublish(boolean gotoProd) {
         String workspaceFolder = pipelineEnvironment.path_workspace
-        pipelineEnvironment.execute "echo version:"
-        pipelineEnvironment.execute "docker run --rm -v $workspaceFolder:/usr/src/mymaven -w /usr/src/mymaven -v '${pipelineEnvironment.homeFolderJenkins}/.m2':/root/.m2 ${pipelineEnvironment.buildImage} mvn help:evaluate -Dexpression=project.version -q -DforceStdout"
 
         if (pipelineEnvironment.buildImage != null) {
             pipelineEnvironment.execute "docker run --rm -v $workspaceFolder:/usr/src/mymaven -w /usr/src/mymaven -v '${pipelineEnvironment.homeFolderJenkins}/.m2':/root/.m2 ${pipelineEnvironment.buildImage} mvn clean install -DskipTests -Dhendelse.environments=${pipelineEnvironment.fetchEnvironment()} -B -e"
