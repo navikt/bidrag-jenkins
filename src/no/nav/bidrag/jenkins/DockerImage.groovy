1d7ce8c37086f7baa320469d6ea8850f5fb15932
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-01-29 13:49:12 +0100

    mvn is running via docker

--- src/no/nav/bidrag/jenkins/DockerImage.groovy
+++ src/no/nav/bidrag/jenkins/DockerImage.groovy
@@ -26,7 +26,7 @@ class DockerImage {
     void releaseAndPublish(boolean gotoProd) {
         String workspaceFolder = pipelineEnvironment.path_workspace
         pipelineEnvironment.execute "echo version:"
-        pipelineEnvironment.execute "mvn help:evaluate -Dexpression=project.version -q -DforceStdout"
+        pipelineEnvironment.execute "docker run --rm -v $workspaceFolder:/usr/src/mymaven -w /usr/src/mymaven mvn help:evaluate -Dexpression=project.version -q -DforceStdout"
 
         if (pipelineEnvironment.buildImage != null) {
             if (!gotoProd) {
